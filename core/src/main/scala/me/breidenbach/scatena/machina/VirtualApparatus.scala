5c9623ba79b1ff15d3d8653f8743c6c2abe6270c
Kevin Breidenbach <kbreiden@kcg.com>
2017-01-06 19:23:28 -0600

    code clean up

--- core/src/main/scala/me/breidenbach/scatena/machina/VirtualApparatus.scala
+++ core/src/main/scala/me/breidenbach/scatena/machina/VirtualApparatus.scala
@@ -1,18 +1,9 @@
 package me.breidenbach.scatena.machina
 
-import me.breidenbach.scatena.apparitio.Apparitio
-import me.breidenbach.scatena.objecto.Objecto
-
-import scala.collection.mutable
-
 /**
   * @author Kevin Breidenbach 
   * Date: 11/17/16
   */
 object VirtualApparatus {
-  val objects = new mutable.HashSet[Objecto]()
-  val applications = new mutable.HashSet[Apparitio]()
-
-
 
 }
--- core/src/main/scala/me/breidenbach/scatena/objecto/Objecto.scala
+++
@@ -1,9 +0,0 @@
-package me.breidenbach.scatena.objecto
-
-/**
-  * @author Kevin Breidenbach 
-  * Date: 11/17/16
-  */
-trait Objecto {
-
-}
